519c27022305947a4fe52bcb1bed1f786fcab313
Alejandro Guanoluisa <xavier.guanoluisa@epn.edu.ec>
2025-04-30 18:28:31 -0500

    Alejandro Guanoluisa

--- src/Ejercicio3/Jugadores.java
+++ src/Ejercicio3/Jugadores.java
@@ -1,5 +1,21 @@
 package Ejercicio3;
 //Creacion clase Jugadores
 public class Jugadores {
-
+    public String nombre;
+    public int numero;
+    public String posicion;
+    public Jugadores(String nombre,int numero,String posicion){
+        this.nombre = nombre;
+        this.numero = numero;
+        this. posicion = posicion;
+    }
+    public void mostrarInfo(){
+        System.out.print("Nombre del jugador: "+nombre);
+        System.out.print("\nNumero que identifica al jugador en el equipo : "+numero);
+        System.out.print("\nPosición en la que juega: "+posicion);
+    }
+    public  void posicionActualizada(String nuevaPosicion){
+        this.posicion = nuevaPosicion;
+        System.out.print("\nLa posicion del jugador "+nombre+" ha sido actualizada a: "+posicion+"\n");
+    }
 }
--- src/Ejercicio3/gestionJugadores.java
+++ src/Ejercicio3/gestionJugadores.java
@@ -2,5 +2,9 @@ package Ejercicio3;
 //Ejercicio 3: Gestión de Jugadores 3
 public class gestionJugadores {
     public static void main(String[] args) {
+        Jugadores jugadores1 = new Jugadores("Cristiano Ronaldo",7,"Delantero");
+        jugadores1.mostrarInfo();
+        jugadores1.posicionActualizada("Defensa");
+        jugadores1.mostrarInfo();
     }
 }
