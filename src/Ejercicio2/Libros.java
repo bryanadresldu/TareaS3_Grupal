81c27bb3710562d76d57a2ef4b50523e02b044e4
POOejercicios <bryan.angulo01@epn.edu.ec>
2025-04-30 19:18:54 -0500

    Ejercicio 2 completo

--- src/Ejercicio2/Libros.java
+++ src/Ejercicio2/Libros.java
@@ -1,4 +1,27 @@
 package Ejercicio2;
 //Creacion clase Libros
 public class Libros {
+    public String titulo;
+    public String autor;
+    public int anioPublicacion;
+    //Constructor
+    public Libros(String titulo, String autor, int anioPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+    }
+    //Metodo para mostrar informacion
+    public void mostrarInformacion(){
+        System.out.println("\n-------------  Informacion del libro  -------------");
+        System.out.println("Nombre del libro: "+titulo);
+        System.out.println("Autor: "+autor);
+        System.out.println("Anio de publicacion: "+anioPublicacion);
+    }
+    //Metodo para actualizar actualizar el año de publicación
+    public void establecerAnio(int nuevoAnio){
+        anioPublicacion=nuevoAnio;
+        System.out.println("-------------  Informacion actualizada -------------");
+        System.out.println("Anio de publicacion actualizados:" + anioPublicacion);
+        System.out.println("----------------------------------------------------");
+    }
 }
--- src/Ejercicio2/gestionLibros.java
+++ src/Ejercicio2/gestionLibros.java
@@ -3,5 +3,14 @@ package Ejercicio2;
 public class gestionLibros {
 
     public static void main(String[] args) {
+        //Creacion de los objetos libros
+        Libros libro1=new Libros("Don Quijote","Miguel de Cervantes",1605);
+        Libros libro2=new Libros("Adios a las armas","Ernest Hemingway",1929);
+        //Llamar a los metodos mostrar informacion
+        // y llamar al metodo establecer anio para actulizar anio de publicacion
+        libro1.mostrarInformacion();
+        libro1.establecerAnio(1620);
+        libro2.mostrarInformacion();
+        libro2.establecerAnio(1950);
     }
 }
