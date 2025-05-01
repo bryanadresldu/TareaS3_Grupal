a6f0375b3a5a371271f5bc6cb36e5b48fdab3260
HanlysNat <137083744+HanlysNat@users.noreply.github.com>
2025-04-30 22:28:03 -0500

    Modif 2-Henry_Tonato

--- src/Ejercicio1/gestionCursos.java
+++ src/Ejercicio1/gestionCursos.java
@@ -1,6 +1,11 @@
 package Ejercicio1;
+
+import Ejercicio1.Cursos;
+
 // Ejercicio 1: Gestión de Cursos
 public class gestionCursos {
      public static void main(String[] args) {
+          Cursos curso = new Cursos("Programacion I","INF101",5);
+          curso.MostrarInfo();
      }
 }
